4e494efac559881e842da480c8d4b16121e52891
bllli <19375623+bllli@users.noreply.github.com>
2024-12-18 02:12:35 +0800

    Update dependencies and add FastAPI Redis integration tests
    - Added FastAPI and Redis as dependencies in `pyproject.toml`, `requirements-dev.lock`, and `requirements.lock`.
    - Introduced new integration tests for FastAPI with Redis in `playgroud/di/test_fastapi_redis.py`, including multiple test cases for the root endpoint.
    - Configured pytest options for asyncio in `pyproject.toml` to ensure proper handling of asynchronous tests.

---
+++ playgroud/di/test_fastapi_redis.py
@@ -0,0 +1,36 @@
+import asyncio
+from httpx import ASGITransport, AsyncClient
+import pytest
+import pytest_asyncio
+
+from playgroud.di.fastapi_redis import create_app
+
+
+@pytest_asyncio.fixture(scope="session")
+async def client():
+    app = create_app()
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        yield ac
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_root(client):
+    response = await client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"result": "locked"}
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_root_2(client):
+    response = await client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"result": "locked"}
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_root_3(client):
+    response = await client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"result": "locked"}
